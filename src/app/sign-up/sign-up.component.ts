b823bd3c820132055a92cdbb340d700c2bc9f791
Marko Bojic <markobo@ccbill.com>
2018-09-28 16:02:03 +0200

    Create initial reactive form

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from './user.model';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -8,16 +8,38 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
+  signupForm: FormGroup;
   user = new User();
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.signupForm = this.fb.group({
+      firstname: '',
+      lastname: '',
+      email: '',
+      sendCatalog: true,
+    })
   }
 
-  onSubmit(sgForm: NgForm) {
-    console.log(sgForm.form);
-    console.log(`Saved ${JSON.stringify(sgForm.value)}`);
+  onSubmit() {
+    console.log(this.signupForm);
+    console.log(`Saved ${JSON.stringify(this.signupForm.value)}`);
+  }
+
+  onSuperUser() {
+    this.signupForm.setValue({
+      firstname: 'Marko',
+      lastname: 'Bojic',
+      email: 'bojic.marko021@gmail.com',
+      sendCatalog: false,
+    })
+  }
+
+  onSetDefaultName() {
+    this.signupForm.patchValue({
+      firstname: 'Marko'
+    })
   }
 
 }
